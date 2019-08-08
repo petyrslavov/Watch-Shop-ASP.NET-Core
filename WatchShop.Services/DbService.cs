f8a66d06de1807806a831352892a4472a16efd50
petyrslavov <petyr_slavov@abv.bg>
2019-08-09 00:39:26 +0300

    Updated Identity pages

--- WatchShop.Services/DbService.cs
+++ WatchShop.Services/DbService.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using WatchShop.Web.Data;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using WatchShop.Web.Data;
 
-namespace WatchShop.Services
-{
-    public abstract class DbService
-    {
-        protected WatchShopDbContext context { get; set; }
+//namespace WatchShop.Services
+//{
+//    public abstract class DbService
+//    {
+//        protected WatchShopDbContext context { get; set; }
 
-        protected DbService(WatchShopDbContext context)
-        {
-            this.context = context;
-        }
-    }
-}
+//        protected DbService(WatchShopDbContext context)
+//        {
+//            this.context = context;
+//        }
+//    }
+//}
